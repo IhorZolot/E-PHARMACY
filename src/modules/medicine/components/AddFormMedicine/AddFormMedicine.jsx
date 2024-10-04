d08a978b05f611f9588e98d0066fde6138bdca17
IhorZolot <fedonn@ukr.net>
2024-10-05 01:10:02 +0300

    add Add formModal styles

--- src/modules/medicine/components/AddFormMedicine/AddFormMedicine.jsx
+++ src/modules/medicine/components/AddFormMedicine/AddFormMedicine.jsx
@@ -1,41 +1,36 @@
 import { SpriteSVG } from '../../../../assets/icons/SpriteSVG'
-
 import styles from './AddFormMedicine.module.scss'
-import twoPills from '../../../../assets/icons/twoPills.svg'
+import twoPills from '../../../../assets/icons/twoPills.png'
+import ModalButton from '../ModalButton/ModalButton'
 
 const AddFormMedicine = () => {
 	return (
-		<>
 			<form className={styles.addFormMedicine}>
 				<h2 >Add medicine to store</h2>
 				<div className={styles.addImageBox}>
-          <div className={styles.addImage}>
-          <img src={twoPills} alt='twoPills'/>
-          </div>
-					
-					<button>
+          <img src={twoPills} alt='twoPills' />
+					<button >
 						<SpriteSVG name='upload' />
 						Upload image
 					</button>
 				</div>
-				<div>
+				<div className={styles.inputBox}>
 					<label htmlFor='name'>Medicine Name</label>
 					<input type='text' name='name' id='name' placeholder='Enter text' />
 				</div>
-				<div>
+				<div className={styles.inputBox}>
 					<label htmlFor='price'>Price</label>
 					<input type='number' name='price' id='price' placeholder='Enter text' />
 				</div>
-				<div>
+				<div className={styles.inputBox}>
 					<label htmlFor='description'>Description</label>
 					<textarea name='description' id='description' placeholder='Enter text' />
 				</div>
-				<div>
-					<button type='submit'>Add medicine</button>
-					<button type='reset'>Cancel</button>
+				<div className={styles.addInputButtonBox}>
+					<ModalButton className={styles.submitButton} type='submit'>Add medicine</ModalButton>
+					<ModalButton className={styles.cancelButton} type='reset'>Cancel</ModalButton>
 				</div>
 			</form>
-		</>
 	)
 }
 
