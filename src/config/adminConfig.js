f23e8d023e0295e1b73f8de51380224fb2c66a7b
IhorZolot <fedonn@ukr.net>
2025-02-19 23:32:18 +0200

    update API

--- src/config/adminConfig.js
+++ src/config/adminConfig.js
@@ -5,13 +5,13 @@ export const API = axios.create({
 	baseURL:
 		import.meta.env.MODE === 'development'
 			? 'http://localhost:3000/api'
-			: 'https://e-pharmacy-backend-b23n.onrender.com/api',
+			: 'https://e-pharmacy-backend-ni8n.onrender.com/api',
 })
 
-export const setToken = (token) => {
-  API.defaults.headers.common.Authorization = `Bearer ${token}`;
-};
+export const setToken = token => {
+	API.defaults.headers.common.Authorization = `Bearer ${token}`
+}
 
 export const removeToken = () => {
-  API.defaults.headers.common.Authorization = '';
-};
+	API.defaults.headers.common.Authorization = ''
+}
